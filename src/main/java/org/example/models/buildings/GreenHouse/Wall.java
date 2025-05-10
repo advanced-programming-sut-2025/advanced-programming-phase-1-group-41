17b85daf455b1fc2dc62d08fb41a5d1ab58923c7
Shahab <shahabahmadloo@yahoo.com>
2025-05-10 16:21:31 +0330

    preCheck added.

---
+++ src/main/java/org/example/models/buildings/GreenHouse/Wall.java
@@ -0,0 +1,7 @@
+package org.example.models.buildings.GreenHouse;
+
+import org.example.models.ObjectMap;
+
+public class Wall implements ObjectMap {
+    //TODO
+}
---
+++ src/main/java/org/example/models/buildings/GreenHouse/WaterTank.java
@@ -0,0 +1,7 @@
+package org.example.models.buildings.GreenHouse;
+
+import org.example.models.ObjectMap;
+
+public class WaterTank implements ObjectMap {
+    //TODO
+}
