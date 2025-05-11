77488b5be6716d6ed383505c60707d9f8c7bcab7
sepica <sepehr.kardel@gmail.com>
2025-05-11 16:10:17 +0330

    fishing rod built

--- src/main/java/org/example/models/tools/FishingRod.java
+++ src/main/java/org/example/models/tools/FishingRod.java
@@ -5,15 +5,25 @@ import org.example.models.skills.Skill;
 
 public class FishingRod implements Tool {
     private Skill usage;
-
+    private FishingRodLevel level;
     public FishingRod() {
         this.usage = Skill.Fishing;
+        this.level = FishingRodLevel.Training;
     }
 
     public Skill getUsage() {
         return usage;
     }
 
+
+    public FishingRodLevel getLevel() {
+        return level;
+    }
+
+    public void increaseLevel() {
+        int nextLevel = Math.max(level.ordinal()+1,FishingRodLevel.values().length - 1);
+        level = FishingRodLevel.values()[nextLevel];
+    }
 
     @Override
     public String getName() {
