3125274b5a700fb44abeea057ce9ee6238baff7c
AIDGH <izadidoostarad@gmail.com>
2025-05-14 11:46:53 +0330

    RelationShip.

--- src/main/java/org/example/controllers/FriendshipController.java
+++ src/main/java/org/example/controllers/FriendshipController.java
@@ -14,6 +14,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 
 public class FriendshipController {
-    public Result friendship(Matcher matcher) {
+    public Result friendshipsList(Matcher matcher) {
+        return null;
     }
 }
--- src/main/java/org/example/views/subGames/FriendshipView.java
+++ src/main/java/org/example/views/subGames/FriendshipView.java
@@ -1,17 +1,18 @@
 package org.example.views.subGames;
 
 import org.example.controllers.AnimalController;
+import org.example.controllers.FriendshipController;
 import org.example.views.commands.gameCommands.AnimalCommands;
 import org.example.views.commands.gameCommands.FriendShipCommands;
 
 import java.util.regex.Matcher;
 
 public class FriendshipView {
-    static AnimalController controller =  new AnimalController();
+    static FriendshipController controller =  new FriendshipController();
     public static boolean check(String input){
         Matcher matcher;
         if((matcher = FriendShipCommands.Friendships.getMatcher(input))!=null){
-            System.out.println(controller.build(matcher));
+            System.out.println(controller.friendshipsList(matcher));
         }else if((matcher = AnimalCommands.BuyAnimal.getMatcher(input))!=null) {
 
         }else{
