3bc2986dc33648a876c8c475da23c79bb0554276
sepica <sepehr.kardel@gmail.com>
2025-07-25 19:29:25 +0330

    change the stuff in this

--- core/src/main/java/com/CEliconValley/common/MachineData.java
+++ core/src/main/java/com/CEliconValley/common/MachineData.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MachineData {
-
+    // testing..
     String craftableMachineName;
     int processTime;
     SlotData produceData;
